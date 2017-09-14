f4ea311e686887c6c3be0019b53619352b405e0d
YoYoMagicRoss <trickstertournaments@gmail.com>
2017-09-14 16:16:29 -0700

    added button

--- project/App.js
+++ project/App.js
@@ -1,22 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export class OpenScreen extends React.Component {
+  handlePress() {
+    console.log("lol");
+      <text>You really know how to push my buttons!</text>
+  }
+
+render() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to Consensus.</Text>
+      <Text>Here to personalize your car experience.</Text>
+      <Button
+      onPress={() => this.handlePress()}
+      title="Learn More"
+      color="#841584"
+      accessibilityLabel="Learn more about this purple button"
+      />
+    </View>
+  );
+}
+}
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Welcome to Consensus.</Text>
-        <Text>Here to personalize your car experience.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+  
+render() {
+  return (
+    <OpenScreen />
+  );
+}
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#bf3',
+    backgroundColor: '#bbb',
     alignItems: 'center',
     justifyContent: 'center',
   },
